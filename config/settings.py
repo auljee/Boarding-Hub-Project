2a2d6ccd7f0cb969513949b8199800c72e46d9c7
auljee <153240119+auljee@users.noreply.github.com>
2025-05-27 20:28:36 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'boardinghub',
-        'USER': 'root',  
-        'PASSWORD': 'ljeeaure@082924',     
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
